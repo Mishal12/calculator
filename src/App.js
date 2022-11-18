96ff21be9afa11540e98f946cf412f6b5de51154
mishal008 <mistye999@gmail.com>
2022-11-18 12:19:59 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import {
+  atan2, chain, derivative, e, evaluate, log, pi, pow, round, sqrt
+} from 'mathjs'
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState('')
+  const buttonNum = ['C', '+-', '%', '/', '7', '8', '9', '*', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.']
+  const equal = ['=']
+
+  const btnHandler = (button) => {
+   // alert("click")
+    setCount(count + button)
+    if (button === "C") {
+      setCount("")
+    }else if(button === '=' && count!=='' ){
+      setCount(evaluate(count))
+    }else if(button === '=' && count ===''){
+      setCount("set something")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='screen'>
+        <Calc display={count}   />
+      </div>
+      <div className='btn'>
+        <Button array={buttonNum}  btnFunction={btnHandler} />
+      </div>
+      <div className='equal'>
+        <Equal array={equal}  btnFunction={btnHandler} />
+      </div>
     </div>
-  );
+  )
+}
+
+const Calc = (props) => {
+  return (
+    <>
+      <h1>{props.display}</h1>
+    </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      {props.array.map((button, index) => {
+        return (
+          <button key={index} onClick={()=>props.btnFunction(button)} >{button}</button>
+        )
+      })}
+    </div>
+  )
+}
+
+const Equal = (props) => {
+  return (
+    <div>
+      {props.array.map((button, index) => {
+        return (
+          <button key={index} onClick={()=>props.btnFunction(button)}>{button}</button>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App;
